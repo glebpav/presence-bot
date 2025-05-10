bffbe0ae1f4cf82f36c3da22107d983335fbb663
hlebnoe_pole <glebpavlyuk@gmail.com>
2025-05-10 22:59:13 +0300

    feat: add menu for basic commands

--- src/main/java/com/xelari/presencebot/telegram/config/BotInit.java
+++ src/main/java/com/xelari/presencebot/telegram/config/BotInit.java
@@ -7,8 +7,12 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.List;
 
 @Component
 @Slf4j
@@ -23,11 +27,32 @@ public class BotInit {
 
     @EventListener({ContextRefreshedEvent.class})
     public void init() throws TelegramApiException {
+
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
             telegramBotsApi.registerBot(telegramBot);
         } catch (TelegramApiException e) {
             log.error(e.getMessage());
         }
+
+        SetMyCommands setMyCommands = new SetMyCommands();
+
+        List<BotCommand> commands = List.of(
+                new BotCommand("/start", "Register and start bot"),
+                new BotCommand("/help", "Show help"),
+                new BotCommand("/meeting", "Meetings options"),
+                new BotCommand("/team", "Teams options")
+        );
+
+        setMyCommands.setCommands(commands);
+
+        try {
+            telegramBot.execute(setMyCommands);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+
     }
+
+
 }
