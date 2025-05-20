32ae0098e8f36849c7f3cea9f8cb114652d4b7b6
hlebnoe_pole <glebpavlyuk@gmail.com>
2025-05-21 00:40:43 +0300

    feat: add profiles for service start

--- src/test/java/com/xelari/presencebot/PresenceBotApplicationTests.java
+++ src/test/java/com/xelari/presencebot/PresenceBotApplicationTests.java
@@ -3,7 +3,7 @@ package com.xelari.presencebot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class PresenceBotApplicationTests {
 
     @Test
